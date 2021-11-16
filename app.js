5153e8630bc5942b794b7552fb7c209491ebfd73
Bastien Roques <bastien.roques971@gmail.com>
2021-11-16 10:37:06 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const buttons = document.querySelectorAll('.btn');
+const result = document.getElementById('result');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        result.textContent += e.target.id;
+    });
+});
+
+equal.addEventListener('click', () => {
+    result.textContent = eval(result.textContent);
+});
+
+clear.addEventListener('click', () => {
+    result.textContent = '';
+});
+
+// let total = 0;
+
+// function addition(x) {
+//     total += x;
+//     return total;
+// }
+
+// function subtraction(x) {
+//     total -= x;
+//     return total;
+// }
+
+// function multiplication(x) {
+//     if (total === 0) {
+//         total = x;
+//     } else {
+//         total *= x;
+//     }
+//     return total;
+// }
+
+// function division(x) {
+//     if (total === 0) {
+//         total = x;
+//     } else {
+//         total /= x;
+//     }
+//     return total;
+// }
+
+// function reset() {
+//     total = 0;
+// }
